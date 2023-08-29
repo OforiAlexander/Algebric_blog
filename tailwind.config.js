185a13dce4be72f76194fd712b7ce24fe1246560
OforiAlexander <alexanderofori07@gmail.com>
2023-08-29 14:50:38 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}",
+  "./node_modules/tw-elements/dist/js/**/*.js"
+],
+  theme: {
+    extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      }
+    },
+  },
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+}
